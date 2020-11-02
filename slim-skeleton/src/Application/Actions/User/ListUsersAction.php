40528cf78907c09b258d95d7f92a873bbcf70a23
Priscilla Joly <priscilla.joly@epitech.eu>
2020-11-02 18:15:56 +0100

    optimize import

--- slim-skeleton/src/Application/Actions/User/ListUsersAction.php
+++ slim-skeleton/src/Application/Actions/User/ListUsersAction.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace App\Application\Actions\User;
 
 use App\Application\Actions\Action;
-use App\Model\User;
 use App\Repository\UserRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Log\LoggerInterface;
