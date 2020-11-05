699fd3fef35049d5cc11766f822b7136f1207467
Priscilla Joly <priscilla.joly@epitech.eu>
2020-11-05 18:06:49 +0100

    lint js file

--- api_express/src/models/discussion.model.js
+++ api_express/src/models/discussion.model.js
@@ -1,58 +1,60 @@
-const Joi = require('@hapi/joi');
-const mongoose = require('mongoose');
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
 
 const Messages = new mongoose.Schema({
-    id_messages: {
-        type: Number,
-        required: true,
-    },
-    id_senders: {
-        type: Number,
-        required: true,
-    },
-    id_receiver: {
-        type: Number,
-        required: true,
-    }
-})
+  id_messages: {
+    type: Number,
+    required: true,
+  },
+  id_senders: {
+    type: Number,
+    required: true,
+  },
+  id_receiver: {
+    type: Number,
+    required: true,
+  },
+});
 
-const Discussion = mongoose.model('Discussion', new mongoose.Schema({
-    id: {
+const Discussion = mongoose.model(
+  "Discussion",
+  new mongoose.Schema(
+    {
+      id: {
         type: Number,
-    },
-    room: {
+      },
+      room: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 100,
-        unique: true
+        unique: true,
+      },
+      messages: [Messages],
     },
-    messages: [Messages]
-}, { timestamps: true }));
+    { timestamps: true }
+  )
+);
 
-function Validate (discussion) {
-    const schema = Joi.object({
-        id: Joi.number().integer(),
-        room: Joi.string()
-            .min(5)
-            .max(100)
-            .required(),
-    });
-    return schema.validate(discussion);
+function Validate(discussion) {
+  const schema = Joi.object({
+    id: Joi.number().integer(),
+    room: Joi.string().min(5).max(100).required(),
+  });
+  return schema.validate(discussion);
 }
 
-function ValidateMessage (message) {
-    const schema = Joi.object({
-        id_messages: Joi.number().integer(),
-        id_senders: Joi.number().integer(),
-        id_receiver: Joi.number().integer(),
-
-    });
-    return schema.validate(message);
+function ValidateMessage(message) {
+  const schema = Joi.object({
+    id_messages: Joi.number().integer(),
+    id_senders: Joi.number().integer(),
+    id_receiver: Joi.number().integer(),
+  });
+  return schema.validate(message);
 }
 
 module.exports = {
-    Discussion,
-    Validate,
-    ValidateMessage
+  Discussion,
+  Validate,
+  ValidateMessage,
 };
